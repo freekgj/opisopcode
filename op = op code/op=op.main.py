d371913e027ca9312fb37068d6fc098722a01ccf
freek <freekgj@hotmail.com>
2019-02-13 11:24:22 +0100

    Project op = op

--- op = op code/op=op.main.py
+++ op = op code/op=op.main.py
@@ -9,29 +9,33 @@ visitors = db.sessions.find()
 category1 = 'price'
 subcategory1 = 'selling_price'
 
-def getpriceproduct(file, subject1, subject2):
+def get_first_product_price(file, subject1, subject2):
     for word in file:
         if subject1 in word:
             subject = (word[subject1])
             if subject2 in subject:
                 return subject[subject2]
 
-def getfirstproductwithX(subject, letter):
+def get_first_product_with_X(subject, letter):
     for word in subject:
         if 'name' in word:
             catogory = word['name']
             if catogory[0] == letter:
                 return catogory
 
-#def gemiddelde_prijs_alle_producten():
+def gemiddelde_prijs_alle_producten(file):
+    listprice = []
+    total = 0
+    for word in file:
+        if "price" in word:
+            price = (word["price"])
+            if "selling_price" in price:
+                listprice.append(price["selling_price"])
+    for product in listprice:
+        total += product
+    avg = total / len(listprice)
+    return avg
 
-
-for word in products:
-    print(word)
-
-
-getpriceproduct(products, category1, subcategory1)
-#getfirstproductwithX(products, "h")
-#gemiddelde_prijs_alle_producten()
-
-print("hello")
+print(get_first_product_price(products, category1, subcategory1))
+print(get_first_product_with_X(products, "h"))
+print(gemiddelde_prijs_alle_producten(products))
