a1c3abda1344b91763d05da124ed2e99458a9160
Freek GJ <43374500+freekgj@users.noreply.github.com>
2019-02-14 14:37:43 +0100

    Update db.py

--- op = op code/db.py
+++ op = op code/db.py
@@ -2,7 +2,7 @@ import pymysql
 import pymysql.cursors
 mydb = pymysql.connect(host='127.0.0.1',
                       user='root',
-                      passwd='zaq1xsw2',
+                      passwd='********',
                       database="op_is_op_database"
                       )
 
